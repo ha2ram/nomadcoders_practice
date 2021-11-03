3d8ccc26d90ba3fc1ed4d0df2958c10a10ce1ff2
root <root@DESKTOP-STMN5G5.localdomain>
2021-11-03 12:38:30 +0900

    variable

--- momentum/app.js
+++ momentum/app.js
@@ -1,1 +1,23 @@
-console.log('lalalala');
+// Variable
+
+// const and let
+const a = 5;
+const b = 2;
+let myName = 'nico';
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log('hello ' + myName);
+
+myName = 'nicolas';
+
+console.log('your new name is ' + myName);
+
+// Boolean
+// true, false, null, undefined
+const amIFat = null;
+console.log(amIFat);
+
+let something;
+console.log(something);
