a3a73368ddab3fc28c86638a6ba81c85bc3871e3
Haram Jeong <niceharam@gmail.com>
2021-11-09 23:58:09 +0900

    Arrays

--- momentum/app.js
+++ momentum/app.js
@@ -21,3 +21,15 @@ console.log(amIFat);
 
 let something;
 console.log(something);
+
+// Arrays
+
+const daysOfWeek = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
+console.log(daysOfWeek);
+
+// Get item from array
+console.log(daysOfWeek[5]);
+
+// Add one more day to the array
+daysOfWeek.push('sun');
+console.log(daysOfWeek);
