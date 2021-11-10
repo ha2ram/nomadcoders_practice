665152d2569c9569d6c70bba802c7d67a229ec0e
Haram Jeong <niceharam@gmail.com>
2021-11-10 10:51:37 +0900

    Objects

--- momentum/app.js
+++ momentum/app.js
@@ -33,3 +33,22 @@ console.log(daysOfWeek[5]);
 // Add one more day to the array
 daysOfWeek.push('sun');
 console.log(daysOfWeek);
+
+// Objects
+const playerName = 'nico';
+const playerPoints = 121212;
+const playerHansome = false;
+const playerFat = 'little bit';
+
+const player = {
+  name: 'nico',
+  points: 10,
+  fat: true,
+};
+
+console.log(player);
+console.log(player.name);
+console.log(player['name']);
+
+player.fat = false;
+console.log(player);
