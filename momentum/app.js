395d5600cd5469eaf29b7731ad6a884c4520993f
Haram Jeong <niceharam@gmail.com>
2021-11-23 16:57:01 +0900

    Conditionals

--- momentum/app.js
+++ momentum/app.js
@@ -93,3 +93,28 @@ function calculateKrAge(ageOfForeigner) {
 const KrAge = calculateKrAge(age);
 
 console.log(KrAge);
+
+// Conditionals
+const age2 = parseInt(prompt('How old are you?'));
+
+console.log(isNaN(age2));
+
+// if (isNaN(age2)) {
+//   console.log('Please write a number');
+// } else {
+//   console.log('Thank you for writing your age');
+// }
+
+if (isNaN(age2) || age2 < 0) {
+  console.log('Please write a real positive number');
+} else if (age2 < 18) {
+  console.log('You are too young');
+} else if (age2 >= 18 && age2 <= 50) {
+  console.log('You can drink');
+} else if (age2 > 50 && age2 <= 80) {
+  console.log('You should exercise');
+} else if (age2 === 100) {
+  console.log('wow you are wise');
+} else if (age2 > 80) {
+  console.log('You can do whatever you want');
+}
