11847a143a3185fdc892c591fb3460e55e4e5db5
Haram Jeong <niceharam@gmail.com>
2021-11-25 23:54:16 +0900

    Searching For Elements

--- momentum/javascript2.js
+++ momentum/javascript2.js
@@ -5,6 +5,7 @@ console.dir(document);
 
 // HTML in JavaScript
 const title = document.getElementById('title');
+// const title = document.querySelector('#title');
 
 console.dir(title);
 
@@ -12,3 +13,20 @@ title.innerText = 'Got you!';
 
 console.log(title.id);
 console.log(title.className);
+
+// Searching For Elements
+const hellos = document.getElementsByClassName('hello');
+
+console.log(hellos);
+
+const title2 = document.getElementsByTagName('h1'); // array
+
+console.log(title2);
+
+const title3 = document.querySelector('.hello h1');
+
+console.log(title3);
+
+const title4 = document.querySelectorAll('.hello h1'); // array
+
+console.log(title4);
