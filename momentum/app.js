86e6cf46cbe71de7ef2429e9b6e96bfeb5ff9a8c
Haram Jeong <niceharam@gmail.com>
2021-12-02 23:59:26 +0900

    Form Submission

--- momentum/app.js
+++ momentum/app.js
@@ -8,7 +8,16 @@ const loginButton = document.querySelector('#login-form button');
 function onLoginBtnClick() {
   // console.log('click!!!!');
   // console.dir(loginInput);
-  console.log(loginInput.value);
+
+  // console.log(loginInput.value);
+
+  const username = loginInput.value;
+  // if (username === '') {
+  //   alert('Please write your name');
+  // } else if (username.length > 15) {
+  //   alert('Your name is too long');
+  // }
+  console.log(username);
 }
 
 loginButton.addEventListener('click', onLoginBtnClick);
