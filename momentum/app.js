46a8544367ff25d8bc490654eec1a26e2e589ae2
Haram Jeong <niceharam@gmail.com>
2021-12-06 21:33:01 +0900

    Events

--- momentum/app.js
+++ momentum/app.js
@@ -3,7 +3,7 @@
 // const loginButton = loginForm.querySelector('button');
 
 const loginInput = document.querySelector('#login-form input');
-const loginButton = document.querySelector('#login-form button');
+// const loginButton = document.querySelector('#login-form button');
 
 function onLoginBtnClick() {
   // console.log('click!!!!');
@@ -20,4 +20,26 @@ function onLoginBtnClick() {
   console.log(username);
 }
 
-loginButton.addEventListener('click', onLoginBtnClick);
+// loginButton.addEventListener('click', onLoginBtnClick);
+
+// SubmitEvent
+const loginForm = document.querySelector('#login-form');
+
+function onLoginSubmit(event) {
+  // event에 대한 정보를 담아줌
+  event.preventDefault(); // 브라우저의 기본 동작(submit 새로고침)을 막아줌
+  console.log(loginInput.value);
+}
+
+loginForm.addEventListener('submit', onLoginSubmit);
+
+// MouseEvent
+const link = document.querySelector('a');
+
+function handleLinkClick(event) {
+  event.preventDefault();
+  console.log(event);
+  // alert('clicked'); // 브라우저의 모든 동작을 막음
+}
+
+// link.addEventListener('click', handleLinkClick);
