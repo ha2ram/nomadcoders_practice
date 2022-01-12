8cb33ee8bd74cd46f8bd3fc4418e3f223e5c4f95
Haram Jeong <niceharam@gmail.com>
2022-01-12 20:47:41 +0900

    saving the image

--- paintjs/app.js
+++ paintjs/app.js
@@ -3,6 +3,7 @@ const ctx = canvas.getContext('2d');
 const colors = document.getElementsByClassName('jsColor');
 const range = document.getElementById('jsRange');
 const mode = document.getElementById('jsMode');
+const saveBtn = document.getElementById('jsSave');
 
 const INITIAL_COLOR = '2c2c2c';
 const CANVAS_SIZE = 700;
@@ -87,12 +88,27 @@ function handleCanvasClick() {
   }
 }
 
+function handleCM(event) {
+  event.preventDefault();
+}
+
+function handleSaveClick() {
+  const image = canvas.toDataURL();
+  // console.log(image);
+  const link = document.createElement('a');
+  // console.log(link);
+  link.href = image;
+  link.download = 'PaintJS[🎨]';
+  link.click();
+}
+
 if (canvas) {
   canvas.addEventListener('mousemove', onMouseMove);
   canvas.addEventListener('mousedown', startPainting);
   canvas.addEventListener('mouseup', stopPainting);
   canvas.addEventListener('mouseleave', stopPainting);
   canvas.addEventListener('click', handleCanvasClick);
+  canvas.addEventListener('contextmenu', handleCM);
 }
 
 // console.log(Array.from(colors));
@@ -107,4 +123,8 @@ if (range) {
 
 if (mode) {
   mode.addEventListener('click', handleModeClick);
+}
+
+if (saveBtn) {
+  saveBtn.addEventListener('click', handleSaveClick);
 }
