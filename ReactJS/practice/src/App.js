402e2563b0e4b772d3d5ad64cbf7f50cbce39ce4
Haram Jeong <niceharam@gmail.com>
2022-02-02 23:31:11 +0900

    React Router

--- ReactJS/practice/src/App.js
+++ ReactJS/practice/src/App.js
@@ -1,15 +1,15 @@
-import './App.css';
-import ToDoList from './ToDoList';
-import CoinTracker from './CoinTracker';
-import Movie from './Movie';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Detail from './routes/Detail';
+import Home from './routes/Home';
 
 function App() {
   return (
-    <div>
-      {/* <ToDoList /> */}
-      {/* <CoinTracker /> */}
-      <Movie />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/movie" element={<Detail />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ ReactJS/practice/src/routes/Home.js
@@ -0,0 +1,15 @@
+import ToDoList from '../components/ToDoList';
+import CoinTracker from '../components/CoinTracker';
+import Movie from '../components/Movie';
+
+function Home() {
+  return (
+    <div>
+      {/* <ToDoList /> */}
+      {/* <CoinTracker /> */}
+      <Movie />
+    </div>
+  );
+}
+
+export default Home;
