884bca6187504d04131fc62c3042233bed305a29
Haram Jeong <niceharam@gmail.com>
2022-02-03 19:47:45 +0900

    Publishing

--- ReactJS/practice/src/App.js
+++ ReactJS/practice/src/App.js
@@ -4,7 +4,7 @@ import Home from './routes/Home';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/movie/:id" element={<Detail />} />
         <Route path="/" element={<Home />} />
