cb35cfa54bde2db7c5b4e2111dae4b28c180bf26
Haram Jeong <niceharam@gmail.com>
2022-02-04 20:50:24 +0900

    Styles

--- ReactJS/practice/src/components/Movie.js
+++ ReactJS/practice/src/components/Movie.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import styles from './Movie.module.css';
 
 function Movie() {
   const [loading, setLoading] = useState(true);
@@ -21,23 +22,34 @@ function Movie() {
   }, []);
 
   return (
-    <div>
+    <div className={styles.container}>
       {loading ? (
-        <h1>Loading...</h1>
+        <h1 className={styles.loader}>Loading...</h1>
       ) : (
-        <div>
+        <div className={styles.movies}>
           {movies.map((movie) => (
-            <div key={movie.id}>
-              <img src={movie.medium_cover_image} alt={movie.title} />
-              <h2>
-                <Link to={`/movie/${movie.id}`}>{movie.title}</Link>
-              </h2>
-              <p>{movie.summary}</p>
-              <ul>
-                {movie.hasOwnProperty('genres')
-                  ? movie.genres.map((g) => <li key={g}>{g}</li>)
-                  : null}
-              </ul>
+            <div key={movie.id} className={styles.movie}>
+              <img
+                src={movie.medium_cover_image}
+                alt={movie.title}
+                className={styles.movie__img}
+              />
+              <div>
+                <h2 className={styles.movie__title}>
+                  <Link to={`/movie/${movie.id}`}>{movie.title}</Link>
+                </h2>
+                <h3 className={styles.movie__year}>{movie.year}</h3>
+                <p>
+                  {movie.summary.length > 235
+                    ? `${movie.summary.slice(0, 235)}...`
+                    : movie.summary}
+                </p>
+                <ul className={styles.movie__genres}>
+                  {movie.hasOwnProperty('genres')
+                    ? movie.genres.map((g) => <li key={g}>{g}</li>)
+                    : null}
+                </ul>
+              </div>
             </div>
           ))}
         </div>
